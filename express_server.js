20ead7eea08d26058cabb1766ae63ab9d01dcc22
vagrant <mikesnow444@gmail.com>
2019-01-16 18:19:26 +0000

    added body parser, added template for adding new url, server adds input to url database

--- express_server.js
+++ express_server.js
@@ -1,6 +1,9 @@
 var express = require("express");
 var app = express();
 var PORT = 8080;
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 
@@ -9,26 +12,47 @@ var urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  let text = '';
+  let possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 6; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+  }
+  return text;
+}
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//get request: passes url database to urls-index ejs file when path = "/urls"
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
 
+//get request: path to urls new page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+//get request: path to target url givin url's id
 app.get("/urls/:id", (req, res) => {
   let templateVars = { shortURL: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
 
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
+//posts form input from urls new page
+app.post("/urls", (req, res) => {
+  let randomShortURL = generateRandomString();
+  urlDatabase[randomShortURL] = req.body.longURL;
+  res.send("Ok");
 });
 
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
