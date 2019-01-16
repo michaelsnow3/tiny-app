c687856b4e87a3ff16f9ac78973fe6ae299874dd
vagrant <mikesnow444@gmail.com>
2019-01-16 19:45:22 +0000

    login form saves username in cookie

--- express_server.js
+++ express_server.js
@@ -2,7 +2,9 @@ var express = require("express");
 var app = express();
 var PORT = 8080; // default port 8080
 const bodyParser = require("body-parser");
+var cookieParser = require('cookie-parser');
 
+app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
@@ -25,13 +27,17 @@ function generateRandomString() {
 
 //get request: passes url database to urls-index ejs file when path = "/urls"
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    username: req.cookies["username"],
+    urls: urlDatabase
+  };
   res.render("urls_index", templateVars);
 });
 
 //get request: path to urls new page
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = { username: req.cookies["username"] }
+  res.render("urls_new", templateVars);
 });
 
 //posts form input from urls new page
@@ -50,6 +56,7 @@ app.get("/u/:shortURL", (req, res) => {
 //get request: path to target url givin url's id
 app.get("/urls/:id", (req, res) => {
   let templateVars = {
+    username: req.cookies["username"],
     shortURL: req.params.id,
     longURL: urlDatabase[req.params.id]
   };
@@ -75,6 +82,12 @@ app.post('/urls/:id/update', (req, res) => {
   urlDatabase[req.params.id] = req.body.longURL;
   res.redirect("/urls/");
 });
+
+//POST endpoint dealing with user login
+app.post('/login', (req, res) => {
+  res.cookie('username', req.body.username, {});
+  res.redirect('/urls');
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
