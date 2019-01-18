56b8384831c0d54fffc68633438ff6c64945952d
vagrant <mikesnow444@gmail.com>
2019-01-18 00:52:06 +0000

    made necessary changes to meet behaviour requirements

--- express_server.js
+++ express_server.js
@@ -20,11 +20,11 @@ app.set("view engine", "ejs");
 var urlDatabase = {
   "b2xVn2": {
     longURL: "http://www.lighthouselabs.ca",
-    userID: "userRandomID"
+    userID: "mike"
   },
   "9sm5xK": {
     longURL: "http://www.google.com",
-    userID: "user2RandomID"
+    userID: "mike"
   }
 };
 
@@ -89,6 +89,57 @@ app.get("/urls/new", (req, res) => {
   }
 });
 
+//get endpoint: path to target url givin url's id
+app.get("/urls/:id", (req, res) => {
+  let userUrls = urlsForUser(req.session.user_id);
+  let exists = false;
+  //checks if url exists
+  if(urlDatabase[req.params.id]){
+    exists = true;
+  }
+  let templateVars = {
+    user: users[req.session.user_id],
+    shortURL: req.params.id,
+    exists: exists
+  };
+  if(userUrls[req.params.id]) {
+    templateVars.longURL = userUrls[req.params.id].longURL;
+  } else {
+    templateVars.longURL = null;
+  }
+  res.render("urls_show", templateVars);
+});
+
+//get endpoint: redirects to newly created short url page
+app.get("/u/:id", (req, res) => {
+  let longURL = urlDatabase[req.params.id].longURL;
+  res.redirect(longURL);
+});
+
+//GET endpoint: allows user to login
+app.get('/login', (req, res) => {
+  let templateVars = { user: users[req.session.user_id] };
+  if(req.session.user_id){
+    //logged in
+    res.redirect("/urls");
+  } else {
+    //not logged in
+    res.render('login', templateVars);
+  }
+});
+
+//GET endpoint: returns page with form containing email and password inputs
+app.get('/register', (req, res) => {
+  let templateVars = { user: users[req.session.user_id] };
+  if(req.session.user_id){
+    //logged in
+    res.redirect("/urls");
+  } else {
+    //not logged in
+    res.render('register', templateVars);
+  }
+});
+
 //posts form input from urls new page
 app.post("/urls", (req, res) => {
   let randomShortURL = generateRandomString();
@@ -99,51 +150,32 @@ app.post("/urls", (req, res) => {
   res.redirect(`/urls/${randomShortURL}`);
 });
 
-//get endpoint: redirects to newly created short url page
-app.get("/u/:shortURL", (req, res) => {
-  let longURL = urlDatabase[req.params.shortURL].longURL;
-  res.redirect(longURL);
-});
-
-//get endpoint: path to target url givin url's id
-app.get("/urls/:id", (req, res) => {
-  let userUrls = urlsForUser(req.session.user_id);
-  let templateVars = {
-    user: users[req.session.user_id],
-    shortURL: req.params.id
-  };
-  if(userUrls[req.params.id]) {
-    templateVars.longURL = userUrls[req.params.id].longURL;
-  } else {
-    templateVars.longURL = null;
-  }
-  res.render("urls_show", templateVars);
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-//delete URL from url database and redirect to urls_index
-app.post('/urls/:id/delete', (req, res) => {
-  if(urlDatabase[req.params.id].userID === req.session.user_id){
-    delete urlDatabase[req.params.id];
-  }
-  res.redirect('/urls');
-});
-
 //update long URL from /urls/:id page
 app.post('/urls/:id/update', (req, res) => {
   if(urlDatabase[req.params.id].userID === req.session.user_id){
     urlDatabase[req.params.id].longURL = req.body.longURL;
+    res.redirect("/urls/");
+  } else if(urlDatabase[req.params.id]){
+    res.send("Cannot update other users url ids");
+  } else {
+    res.send("Url not found");
   }
-  res.redirect("/urls/");
 });
 
-//GET endpoint: allows user to login
-app.get('/login', (req, res) => {
-  let templateVars = { user: users[req.session.user_id] };
-  res.render('login', templateVars);
+//delete URL from url database and redirect to urls_index
+app.post('/urls/:id/delete', (req, res) => {
+  if(req.session.user_id){
+    if(urlDatabase[req.params.id].userID === req.session.user_id){
+      delete urlDatabase[req.params.id];
+      res.redirect('/urls');
+    } else if(urlDatabase[req.params.id]){
+      res.send("Cannot delete other users url ids");
+    } else {
+      res.send("Url not found");
+    }
+  } else {
+    res.send("Login to delete your URLs");
+  }
 });
 
 //POST endpoint dealing with user login
@@ -170,12 +202,6 @@ app.post('/logout', (req, res) => {
   req.session = null;
   res.redirect('/urls');
 });
-
-//GET endpoint: returns page with form containing email and password inputs
-app.get('/register', (req, res) => {
-  let templateVars = { user: users[req.session.user_id] };
-  res.render('register', templateVars);
-})
 
 //POST endpoint that takes in register form input
 app.post('/register', (req, res) => {
